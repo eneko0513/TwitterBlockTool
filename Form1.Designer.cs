f27b6946feba63e1b0fa16804ddb242ed15a8cbd
eneko0513 <ion.n@hotmail.co.jp>
2019-05-08 23:30:50 +0900

    commit
    とりあえず動く感じのやつ。
    ADGVとCoreTweetをnugetから落として利用します。

---
+++ Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace BlockSpecificUsersOnTwitter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.advancedDataGridView_main = new ADGV.AdvancedDataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.bindingSource_main = new System.Windows.Forms.BindingSource(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.advancedDataGridView_main)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource_main)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(951, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.advancedDataGridView_main);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 100);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(951, 445);
+            this.panel2.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(951, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "基本設定";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(47, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.PasswordChar = '*';
+            this.textBox1.Size = new System.Drawing.Size(122, 19);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "PIN：";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "認証";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(184, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(345, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "※一部権限の認証を求められますが、自動ツイートなどは行われません。";
+            // 
+            // advancedDataGridView_main
+            // 
+            this.advancedDataGridView_main.AllowUserToDeleteRows = false;
+            this.advancedDataGridView_main.AutoGenerateContextFilters = true;
+            this.advancedDataGridView_main.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.advancedDataGridView_main.DateWithTime = false;
+            this.advancedDataGridView_main.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.advancedDataGridView_main.Location = new System.Drawing.Point(0, 0);
+            this.advancedDataGridView_main.Name = "advancedDataGridView_main";
+            this.advancedDataGridView_main.ReadOnly = true;
+            this.advancedDataGridView_main.RowTemplate.Height = 21;
+            this.advancedDataGridView_main.Size = new System.Drawing.Size(951, 445);
+            this.advancedDataGridView_main.TabIndex = 0;
+            this.advancedDataGridView_main.TimeFilter = false;
+            this.advancedDataGridView_main.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.AdvancedDataGridView_main_CellClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(93, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "状態：未認証";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 59);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "検索ワード：";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(73, 55);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(454, 19);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(533, 53);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "検索";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(170, 77);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "ブロック";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(12, 77);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "すべてチェック";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Enabled = false;
+            this.button5.Location = new System.Drawing.Point(89, 77);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "チェックを外す";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(951, 545);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Twitterブロックん";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.advancedDataGridView_main)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource_main)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private ADGV.AdvancedDataGridView advancedDataGridView_main;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.BindingSource bindingSource_main;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,194 @@
+﻿using CoreTweet;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BlockSpecificUsersOnTwitter
+{
+    public partial class Form1 : Form
+    {
+        private DataTable _dataTable = null;
+        private DataSet _dataSet = null;
+
+        OAuth.OAuthSession session;
+        public Tokens tokens;
+
+        public Form1()
+        {
+
+            InitializeComponent();
+
+            // データセットの初期化
+            _dataTable = new DataTable();
+            _dataSet = new DataSet();
+
+            // データソースの初期化
+            bindingSource_main.DataSource = _dataSet;
+
+            // データグリッドの初期化
+            advancedDataGridView_main.DataSource = bindingSource_main;
+
+            // バインディングソースの設定
+            SetHeaderData();
+            session = OAuth.Authorize("apikey", "token");
+            advancedDataGridView_main.Columns[3].Width = 470;
+            GetPinCode(session);
+            button2.Enabled = false;
+        }
+
+
+        private void SetHeaderData()
+        {
+            try
+            {
+                _dataTable = _dataSet.Tables.Add("Table");
+                _dataTable.Columns.Add("ブロック対象", typeof(Boolean));
+                _dataTable.Columns.Add("ユーザーID", typeof(string));
+                _dataTable.Columns.Add("ユーザー名", typeof(string));
+                _dataTable.Columns.Add("ツイート内容", typeof(string));
+                bindingSource_main.DataMember = _dataTable.TableName;
+            }
+            catch
+            {
+            }
+        }
+
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //トークンの発行
+            string pincode = textBox1.Text;
+            tokens = OAuth.GetTokens(session, pincode);
+
+            label2.Text = "状態：" + tokens.ScreenName + "として認証済み";
+        }
+
+        private void GetPinCode(CoreTweet.OAuth.OAuthSession session)
+        {
+            System.Diagnostics.Process.Start(session.AuthorizeUri.AbsoluteUri);
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            TweetSearch();
+        }
+
+        private void TweetSearch()
+        {
+            // グローバル検索
+            string keyword = textBox2.Text;
+            if (!keyword.Equals(""))
+            {
+                _dataTable.Clear();
+                button3.Enabled = true;
+                button4.Enabled = true;
+                button5.Enabled = true;
+                var result = tokens.Search.Tweets(count => 100, q => keyword);
+                // countは読み込み数。指定しなければDefoultの数値が入る。
+                foreach (var value in result)
+                {
+                    string scrName = value.User.ScreenName;     // @User_ID
+                    string name = value.User.Name;              // ユーザー名
+                    string text = value.Text;                   // Tweet
+                    AddUserData(false, scrName, name, text);
+                }
+            }
+            else
+            {
+                button3.Enabled = false;
+                button4.Enabled = false;
+                button5.Enabled = false;
+            }
+        }
+
+
+        private void AddUserData(Boolean check, string scrName, string name, string text)
+        {
+            try
+            {
+                object[] newrow = new object[] {
+                        check,
+                        scrName,
+                        name,
+                        text
+                };
+                _dataTable.Rows.Add(newrow);
+                Application.DoEvents();
+            }
+            catch
+            {
+                // StatsBarDisplayChange(1);
+            }
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+            button2.Enabled = textBox2.Text.Equals("") ? false : true;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
+        }
+
+        private void AdvancedDataGridView_main_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if ((e.ColumnIndex == 0) && (e.RowIndex != (-1)))
+            {
+                advancedDataGridView_main.Rows[e.RowIndex].Cells[0].Value = !Convert.ToBoolean(advancedDataGridView_main.Rows[e.RowIndex].Cells[0].Value);
+            }
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < advancedDataGridView_main.RowCount; i++)
+            {
+                advancedDataGridView_main.Rows[i].Cells[0].Value = true;
+            }
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < advancedDataGridView_main.RowCount; i++)
+            {
+                advancedDataGridView_main.Rows[i].Cells[0].Value = false;
+            }
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("ブロックしちゃいます？",
+            "ブロック",
+            MessageBoxButtons.YesNo,
+            MessageBoxIcon.Exclamation,
+            MessageBoxDefaultButton.Button2);
+
+            //何が選択されたか調べる
+            if (result == DialogResult.Yes)
+            {
+                for (int i = 0; i < advancedDataGridView_main.RowCount; i++)
+                {
+                    if (Convert.ToBoolean(advancedDataGridView_main.Rows[i].Cells[0].Value))
+                    {
+                        tokens.Blocks.Create(screen_name => advancedDataGridView_main.Rows[i].Cells[1].Value);
+                    }
+                }
+                DialogResult result2 = MessageBox.Show("ブロックが完了しました",
+                "完了",
+                MessageBoxButtons.OK);
+            }
+            else if (result == DialogResult.No)
+            {
+            }
+            else if (result == DialogResult.Cancel)
+            {
+            }
+        }
+    }
+}
